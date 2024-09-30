4d7eae3868af6da8eed6a123d3e06229f16188c0
LeeTaeHyun <apth1023@naver.com>
2024-09-30 17:06:39 +0900

    최종

--- Source/TimelessAdventure/Item/TA_Arrow.cpp
+++ Source/TimelessAdventure/Item/TA_Arrow.cpp
@@ -2,9 +2,11 @@
 
 
 #include "Item/TA_Arrow.h"
+#include "Player/TA_PlayerCharacter.h"
 
 #include "GameFramework/ProjectileMovementComponent.h"
 #include "Components/SphereComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 ATA_Arrow::ATA_Arrow()
 {
@@ -26,6 +28,8 @@ ATA_Arrow::ATA_Arrow()
 	ProjectileMovementComp->InitialSpeed = 3000.0f;
 	ProjectileMovementComp->MaxSpeed = 5000.0f;
 	ProjectileMovementComp->ProjectileGravityScale = 0.0f;
+
+	AttackDamage = 10.0f;
 }
 
 void ATA_Arrow::BeginPlay()
@@ -33,13 +37,30 @@ void ATA_Arrow::BeginPlay()
 	Super::BeginPlay();
 	
 	ProjectileMovementComp->SetActive(false);
+	SphereComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	SphereComp->OnComponentBeginOverlap.AddDynamic(this, &ATA_Arrow::OnComponentBeginOverlap);
 }
 
 void ATA_Arrow::FireArrow(FVector Pos)
 {
+	SetLifeSpan(3.0f);
 	FVector Direction = (Pos - GetActorLocation()).GetSafeNormal();
 	ProjectileMovementComp->Velocity = Direction * ProjectileMovementComp->InitialSpeed;
 	ProjectileMovementComp->Activate();
+	SphereComp->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+}
+
+void ATA_Arrow::OnComponentBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	// 임시로 플레이어 그냥 가져다 체크
+	if (OtherActor == UGameplayStatics::GetPlayerCharacter(GetWorld(), 0)) return;
+
+	// 데미지 전달
+	UGameplayStatics::ApplyDamage(OtherActor, AttackDamage, GetWorld()->GetFirstPlayerController(), GetWorld()->GetFirstPlayerController()->GetPawn(), UDamageType::StaticClass());
+
+	GEngine->AddOnScreenDebugMessage(97, 2.0f, FColor::Red, TEXT("ArrowAttack"));
+	Destroy();
 }
 
 void ATA_Arrow::Tick(float DeltaTime)
--- Source/TimelessAdventure/Item/TA_Arrow.h
+++ Source/TimelessAdventure/Item/TA_Arrow.h
@@ -23,6 +23,10 @@ public:
 	void FireArrow(FVector Pos);
 
 protected:
+	UFUNCTION()
+	void OnComponentBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+protected:
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<class UStaticMeshComponent> BaseMesh;
 
@@ -37,5 +41,8 @@ protected:
 
 	UPROPERTY(VisibleAnywhere, Category = "Component")
 	TObjectPtr<class UProjectileMovementComponent> ProjectileMovementComp;
+
+	UPROPERTY(EditAnywhere, Category = "Attack")
+	float AttackDamage;
 
 };
